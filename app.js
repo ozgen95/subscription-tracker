306498f598dea50a73d513275c2a3def8eb4d0d8
ozgen95 <ozgen.ates@mail.utoronto.ca>
2026-05-25 17:03:38 -0400

    routing and setting up the database

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 // @ts-check
 import express from "express";
 import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello from Subscription API");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Subscription API running on port http://localhost:${PORT}`);
+  await connectToDatabase();
 });
 
 export default app;
